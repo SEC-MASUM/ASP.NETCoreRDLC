32f99b75a83527734daf9eda87d5ca9e50c52bd0
SEC-MASUM <masum.jess42@gmail.com>
2021-04-03 16:29:56 +0600

    Create Windows Form App Project, To Create Report1.rdlc file  download Microsoft RDLC Report Designer and Install

---
+++ Source/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NETCoreRDLC.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //(2) Create Action Method for Print
+        public IActionResult Print()
+        {
+            return View();
+        }
+    }
+}
